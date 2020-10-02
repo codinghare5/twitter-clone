3be79387a0f31aa31172024b0ba6b45d01303816
Marcin Mukosiej <marcinmukosiej@gmail.com>
2020-10-02 23:01:01 +0100

    continue on feed and header is done

--- src/Feed.js
+++ src/Feed.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import './Feed.css';
 
 function Feed() {
     return (
         <div className="feed">
             {/* Heder */}
-            <h2>Home</h2>
+            <div className="feed__header">
+                <h2>Home</h2>
+            </div>
 
             {/* TweetBox */}
 
