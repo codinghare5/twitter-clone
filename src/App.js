ef976a63e4537214e9b7fc8221daa43d651a2ac9
Marcin Mukosiej <marcinmukosiej@gmail.com>
2020-10-02 14:46:12 +0100

    started working on the left sidebar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
     <div className="app">
-      
+      <Sidebar />
+      {/* Feed */}
+
+      {/* Widgets */}
     </div>
   );
 }
---
+++ src/Sidebar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Sidebar.css'
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            {/* Twotter icon */}
+
+            {/* SidebarOption */}
+            {/* SidebarOption */}
+            {/* SidebarOption */}
+
+            {/* Button -> Twitter */}
+        </div>
+    )
+}
+
+export default Sidebar
