8edf292bce8fbb68d10d16ef29e3af9a14fc268d
Marcin Mukosiej <marcinmukosiej@gmail.com>
2020-10-04 21:18:47 +0100

    framing is done, coding time

--- src/Widgets.js
+++ src/Widgets.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import './Widgets.css'
+import SearchIcon from '@material-ui/icons/Search';
+import { 
+    TwitterTimelineEmbed, 
+    TwitterTweetEmbed,
+} from "react-twitter-embed";
 
 function Widgets() {
     return (
         <div className="widgets">
-            <h2>This is widgets</h2>
+            <div className="widgets__input">
+                <SearchIcon className="widgets__searchIcon"/>
+                <input placeholder="Search Twitter" type="text" />
+            </div>
+
+            <div className="widgets__widgetContainer">
+                <h2>What's happening</h2>
+
+                <TwitterTweetEmbed tweetId={"1293154394058432512"} />
+
+                <TwitterTimelineEmbed
+                    sourceType="profile"
+                    screenName="reactjs"
+                    options={{ height: 400 }}
+                />
+
+            </div>
         </div>
     )
 }
